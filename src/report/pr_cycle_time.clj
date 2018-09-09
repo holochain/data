3fdb7ab9a7e31af88c75d10293af6f9fcf66c47a
David Meister <thedavidmeister@gmail.com>
2018-09-09 20:40:34 +1000

    time series for PR cycle time

--- src/report/pr_cycle_time.clj
+++ src/report/pr_cycle_time.clj
@@ -6,24 +6,25 @@
   time.spec
   github.core
   incanter.charts
-  incanter.core))
+  incanter.core
+  clj-time.coerce))
 
-(spec/def ::start :time.spec/datetime)
-(spec/def ::end :time.spec/datetime)
+(spec/def ::open :time.spec/datetime)
+(spec/def ::close :time.spec/datetime)
 
 (spec/def ::datum
  (spec/keys
   :req
-  [::start
-   ::end]))
+  [::open
+   ::close]))
 
 (defn pr->datum
  [pr]
  {:pre [(spec/valid? :github.spec/pr pr)]
   :post [(spec/valid? ::datum %)]}
- {::start (-> pr :created_at github.core/iso8601->)
+ {::open (-> pr :created_at github.core/iso8601->)
   ; fallback to now if the pr is still open
-  ::end
+  ::close
   (or
    (some-> pr :closed_at github.core/iso8601->)
    (clj-time.core/now))})
@@ -34,15 +35,31 @@
   :post [(spec/valid? int? %)]}
  (clj-time.core/in-hours
   (clj-time.core/interval
-   (::start datum)
-   (::end datum))))
+   (::open datum)
+   (::close datum))))
+
+(defn prs->histogram!
+ [prs]
+ (incanter.core/view
+  (incanter.charts/histogram
+   (map
+    (comp datum->cycle-time pr->datum)
+    prs))))
+
+
+(defn prs->time-series!
+ [prs]
+ (let [open-times (map (comp clj-time.coerce/to-long ::open pr->datum) prs)
+       cycle-times (map (comp datum->cycle-time pr->datum) prs)]
+  (incanter.core/view
+   (incanter.charts/time-series-plot
+    open-times
+    cycle-times))))
 
 (defn do-it!
+ "shows histogram and time series for cycle times for closed PRs"
  [user repo]
  (let [prs (github.prs/all-prs! user repo {:state "closed"})]
   (prn "pr count:" (count prs))
-  (->> prs
-   (map
-    (comp datum->cycle-time pr->datum))
-   incanter.charts/histogram
-   incanter.core/view)))
+  (prs->histogram! prs)
+  (prs->time-series! prs)))
